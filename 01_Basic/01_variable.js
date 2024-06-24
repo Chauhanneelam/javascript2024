bf87254a35ee8bfaab745252451ec79f3c641769
Chauhanneelam <your-neelamchauhannk@gmail.com>
2024-06-24 14:11:29 -0700

    I learn variables

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 1324534
+let accountEmail = "Neelam@gmail.com"
+accountCity = "Uttar pradesh"
+let accountstates;
+
+// accountId =2 not allowed
+
+// Prefer not to use var 
+// because of issue in block scope and functional scpe
+
+accountEmail = "neelamchauhannk@gmail.com"
+accountId = 89879809
+accountCity = "Siswa Bazar"
+
+// console.table  ([accountEmail, accountstates, accountCity])
+console.log("accountstates")
